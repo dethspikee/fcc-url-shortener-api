6ab088474c856082f33bc73a2d6128a3df58bb97
Damian <zettie008@gmail.com>
2020-04-23 23:36:06 +0100

    testing v2

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -38,10 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # 3rd party 
-    'rest_framework',
-
+    # local
     'api.apps.ApiConfig',
 ]
 
